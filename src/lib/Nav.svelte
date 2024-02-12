1b4c873b1f9d22ad501a784ac184c26b4d25cfad
HangBeni <hangbenjamin2003@gmail.com>
2024-02-12 15:53:51 +0100

    feat: Basic navigation

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Links } from "../utils/types";
+
+    export const links : Links[] = [
+    {name: "Főoldal", link: "/"},
+    {name: "Regisztráció", link: "/registration"},
+    {name: "Bejelentkezés", link: "/login"}, //Ha be van jelentkezve akkor cserélje ki egy avatarral
+    //Saját bandám bejelentkezés után ha van a usernek
+    ];
+</script>
+
+<nav>
+    <ul>
+        {#each links as link}
+            <li><a href={link.link}>{link.name}</a></li>
+        {/each}
+    </ul>
+</nav>
