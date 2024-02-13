aae2ab554e40beaaee419a9e13a71ebd563903b7
HangBeni <hangbenjamin2003@gmail.com>
2024-02-13 18:08:22 +0100

    login

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,14 @@
+<form method="POST" >
+    <div class="inputPlaceholder">
+      
+        <label for="userName">User Name</label>
+        <input required type="text" name="userName" id="userName" />
+        <span class="userNameError"></span>
+  
+        <label for="userPsswrd">Password</label>
+        <input required type="text" name="userPsswrd" id="userPsswrd" />
+        <span class="passwordError"></span>
+  
+        <input type="submit" value="Submit" id="submitB" />
+      </div>
+  </form>
