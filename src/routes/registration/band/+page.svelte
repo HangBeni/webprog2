a3b270573a49250ae3d5de426ca12a1250147571
HangBeni <hangbenjamin2003@gmail.com>
2024-02-13 18:08:16 +0100

    initial reg. forms

---
+++ src/routes/registration/band/+page.svelte
@@ -0,0 +1,35 @@
+
+
+<form
+      method="post"
+    >
+      <div class="inputPlaceholder">
+        <label for="bandName">Banda neve</label>
+        <input required type="text" name="bandName" id="bandName" />
+
+        <label for="bandBirth">Banda alakulása</label>
+        <input type="datetime" name="bandBirth" id="bandBirth" />
+
+        <label for="genres">Zsanrák</label>
+        <input required type="text" name="genres" id="genres" />
+
+        <label for="backStory">A banda sztorija, története</label>
+        <textarea
+          required
+          name="backStory"
+          id="backStory"
+          cols="20"
+          rows="10"
+          placeholder="     Your story"
+        ></textarea>
+        <input
+          type="file"
+          accept="image/*"
+          name="bandPicture"
+          id="bandPicture"
+          alt=""
+        />
+        <button type="submit" id="submitB" >
+        </button>
+      </div>
+    </form>
