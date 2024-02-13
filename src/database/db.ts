6279d4cb3a85cf3bf6d056595dc6b5971dbe4358
HangBeni <hangbenjamin2003@gmail.com>
2024-02-13 18:01:54 +0100

    feat: base database template

---
+++ src/database/db.ts
@@ -0,0 +1,69 @@
+import  sqlite3  from "sqlite3";
+
+const DBSOURCE = 'db.sqlite';
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+	if (err) {
+		// Cannot open database
+		console.error(err.message);
+		throw err;
+	} else {
+		console.log('Connected to the SQLite database.');
+
+		db.run(
+			`CREATE TABLE band (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text UNIQUE NOT NULL, 
+            birth text, 
+            genre text NOT NULL, 
+            story text NOT NULL, 
+            CONSTRAINT name_unique UNIQUE (name)
+            )`,
+			(err) => {
+				if (err) {
+					// Table already created
+				} else {
+					// Table just created, creating some rows
+					var insert = 'INSERT INTO band (name, birth, genre, story) VALUES (?,?,?,?)';
+					db.run(insert, [
+						'FFTS',
+						'2021-01-01',
+						'Modern metál, progresszív jegyekkel',
+						'Bsazó emberek'
+					]);
+				}
+			}
+		);
+
+		db.run(
+			`CREATE TABLE user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text UNIQUE NOT NULL, 
+            band text NOT NULL,
+            password text NOT NULL,
+            CONSTRAINT name_unique, password_unique UNIQUE (name, password)
+            )`,
+			(err) => {
+				if (err) {
+					// Table already created
+				} else {
+					var insert = 'INSERT INTO user (name, band, password) VALUES (?,?,?)';
+					db.run(insert, ['Beni', 'FFTS', '123']);
+				}
+			}
+		);
+
+		db.run(
+			`CREATE TABLE posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            author text NOT NULL, 
+            content text NOT NULL,
+            CONSTRAINT id_unique UNIQUE(id)
+        )`, (err:Error) => {
+          
+        }
+		);
+	}
+});
+
+export default db;
