a0aebec7c507c1d4660373bdad1428e0e341eacc
HangBeni <hangbenjamin2003@gmail.com>
2024-02-13 18:03:12 +0100

    feat: basic footer

---
+++ src/lib/reusables/Footer.svelte
@@ -0,0 +1,7 @@
+<footer>
+    <div>
+        <label for="creator">Created by: </label>
+        <h3 id="creator">Hang Benjámin - HRPHEE</h3>
+        <a href="https://github.com/HangBeni/webprog2">Github repo</a>
+    </div>
+</footer> 
