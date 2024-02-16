b3faffe773253fde9c4df2002f70a23db707bb98
HangBeni <hangbenjamin2003@gmail.com>
2024-02-16 10:19:40 +0100

    feat: table with serialize

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,69 +1,70 @@
-import  sqlite3  from "sqlite3";
+import sqlite3 from 'sqlite3';
 
-const DBSOURCE = "db.sqlite";
+const DBSOURCE = 'db.db';
+
 sqlite3.verbose();
+
 let db = new sqlite3.Database(DBSOURCE, (err) => {
-  if (err) {
-    // Cannot open database
-    console.error(err.message);
-    throw err;
-  } else {
-    console.log("Connected to the SQLite database.");
+	if (err) {
+		// Cannot open database
+		console.error(err.message);
+		throw err;
+	} else {
+		console.log('Connected to the SQLite database.');
+	}
+});
 
-    db.run(
-      `CREATE TABLE band (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text UNIQUE NOT NULL, 
-            birth text, 
-            genre text NOT NULL, 
-            story text NOT NULL, 
-            CONSTRAINT name_unique UNIQUE (name)
-            )`,
-      (err) => {
-        if (err) {
-          // Table already created
-        } else {
-          // Table just created, creating some rows
-          var insert =
-            "INSERT INTO band (name, birth, genre, story) VALUES (?,?,?,?)";
-          db.run(insert, ["FFTS", "2021-01-01", "metál", "Bsazó emberek"]);
-        }
-      }
-    );
+db.serialize(() => {
+	db.run(
+		`CREATE TABLE band (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name text NOT NULL, 
+        birth text NOT NULL, 
+        genre text NOT NULL, 
+        story text NOT NULL, 
+        UNIQUE(name)
+        )`,
+		(err) => {
+			if (err) {
+				// Table already created
+			} else {
+				// Table just created, creating some rows
+				var insert = 'INSERT INTO band (name, birth, genre, story) VALUES (?,?,?,?)';
+				db.run(insert, ['FFTS', '2021-01-01', 'metál', 'Bsazó emberek']);
+			}
+		}
+	);
 
-    db.run(
-      `CREATE TABLE user (
+	db.run(
+		`CREATE TABLE user (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text UNIQUE NOT NULL, 
+            name text  NOT NULL, 
             band text NOT NULL,
             password text NOT NULL,
-            CONSTRAINT name_unique, password_unique UNIQUE (name, password)
+            UNIQUE(name, password)
             )`,
-      (err) => {
-        if (err) {
-          // Table already created
-        } else {
-          var insert = "INSERT INTO user (name, band, password) VALUES (?,?,?)";
-          db.run(insert, ["Benjámin", "FFTS", "12345"]);
-        }
-      }
-    );
+		(err) => {
+			if (err) {
+				// Table already created
+			} else {
+				var insert = 'INSERT INTO user (name, band, password) VALUES (?,?,?)';
+				db.run(insert, ['Benjámin', 'FFTS', '12345']);
+			}
+		}
+	);
 
-    db.run(
-      `CREATE TABLE posts (
+	db.run(
+		`CREATE TABLE posts (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             author text NOT NULL, 
             content text NOT NULL,
-            CONSTRAINT id_unique UNIQUE(id)
-        )`,
-      (err) => {
-        if (err) {
-          // Table already created
-        } 
-      }
-    );
-
-  }
+            )`,
+		(err) => {
+			if (err) {
+				// Table already created
+			}
+		}
+	);
 });
 
 export default db;
