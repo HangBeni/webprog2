3b77309c93929926919c64389b07099fec66e0db
HangBeni <hangbenjamin2003@gmail.com>
2024-02-26 16:24:07 +0100

    feat: login out

--- src/routes/registration/user/+page.server.ts
+++ src/routes/registration/user/+page.server.ts
@@ -4,36 +4,7 @@ import db from '../../../database/db';
 
 export const actions = {
 	login: async ({ request }) => {
-		const data = await request.formData();
-		const formData: FormType = {
-			userName: data.get('userName')!.toString(),
-			userPassword: data.get('userPassword')!.toString(),
-			userBand: data.get('userBand')!.toString()
-		};
-		let response: Object | null = null;
-		try {
-			db.get<User>(
-				'SELECT * FROM user WHERE name = ? AND password = ? AND band = ?;',
-				[formData.userName, formData.userPassword, formData.userBand],
-				(err, row) => {
-					if (err || !row) {
-						response = row as User;
-						console.log(row);
-						// console.log(response);
-						// return response;
-					}
-
-					response = row as User;
-					console.log(row);
-					// console.log(response);
-					// return response;
-				}
-			);
-		} catch (error) {
-			console.error('Error while fetching user:', error);
-		}
-		console.log(response);
-		return { success: true, message: 'Hello from the server', body: formData, res: response };
+		
 	},
 
 	registration: async ({ request }) => {
@@ -50,7 +21,7 @@ export const actions = {
 
 			db.run(insert, [formData.userName, formData.userBand, formData.userPassword], (err) => {
 				if (err) {
-					response = { res: '' };
+					response = { res: 'Error' };
 				}
 				response = { res: `It's okay u good ${formData.userName}` };
 			});
