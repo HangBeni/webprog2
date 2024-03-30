b2029dc33ac456bec3f9c738625f78af2e33fca8
HangBeni <hangbenjamin2003@gmail.com>
2024-03-30 11:28:37 +0100

    feat: base comment template

---
+++ src/lib/posts/CommentCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Comment } from '$lib/types';
+
+	export let comment: Comment;
+</script>
+
+<div>
+	<h4>{comment.author}</h4>
+	<p>{comment.content}</p>
+	<i>{Date.parse(comment.created_at)}</i>
+</div>
