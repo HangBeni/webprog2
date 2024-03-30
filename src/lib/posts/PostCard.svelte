b14b99dde9973ce8bc296069b0079e3fcfebd44a
HangBeni <hangbenjamin2003@gmail.com>
2024-03-30 11:29:10 +0100

    feat: Post card with slot

---
+++ src/lib/posts/PostCard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Post } from '$lib/types';
+
+	export let post: Post;
+</script>
+
+<div>
+	<h1>{post.author}</h1>
+	<p>{post.content}</p>
+	<i>{post.created_at}</i>
+	<slot />
+</div>
