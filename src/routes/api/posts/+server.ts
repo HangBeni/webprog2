62453d263a08bfb2710f906f355cd54b8930bba9
HangBeni <hangbenjamin2003@gmail.com>
2024-03-30 11:29:45 +0100

    feat: post get (y)

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -3,14 +3,29 @@ import type { RequestEvent } from '@sveltejs/kit';
 import db from '../../../database/db';
 
 export async function GET() {
-	let sql = 'SELECT * FROM posts;';
-	let posts = await db.all<Post[]>(sql);
-    return await new Response(JSON.stringify(posts), {status: 200});
+	try {
+		let sql = 'SELECT * FROM posts;';
+		let posts = await new Promise((resolve, reject) => {
+			db.all<Post[]>(sql, (err, rows) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(rows);
+				}
+			});
+		});
+	
+		return await new Response(JSON.stringify(posts), { status: 200 });
+		
+	} catch (error) {
+		return await new Response('Server Error', { status: 500 });
+		
+	}
 }
 
-export async function POST(event : RequestEvent) {
-    let post = await event.request.json();
-	let sql = 'INSERT INTO posts (author, content, created_at) VALUES (?, ?, ?);';
-	await db.run(sql, [post.author, post.content, Date.now()]);
-    return await new Response("", {status: 200});
+export async function POST(event: RequestEvent) {
+	let post = await event.request.json();
+	let sql = 'INSERT INTO posts (author, content) VALUES (?, ?);';
+	await db.run(sql, [post.author, post.content]);
+	return await new Response('Success', { status: 200 });
 }
