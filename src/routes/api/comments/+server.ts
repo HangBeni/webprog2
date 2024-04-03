874891151faa5fc28f9ea1ddc79b5b15abd19d1d
HangBeni <hangbenjamin2003@gmail.com>
2024-04-03 11:48:17 +0200

    feat: get and post comments

--- src/routes/api/comments/+server.ts
+++ src/routes/api/comments/+server.ts
@@ -1,16 +1,34 @@
-import db from "../../../database/db";
-import type { Comment } from "$lib/types";
-
+import db from '../../../database/db';
+import type { Comment } from '$lib/types';
+import type { RequestEvent } from '@sveltejs/kit';
 
 export async function GET() {
-    try {
-        let sql = 'SELECT * FROM comments;';
-        let comments = await db.all<Comment[]>(sql);
-        return await new Response(JSON.stringify(comments), {status: 200});
+	try {
+		let sql = 'SELECT * FROM comments;';
+		let comments = await new Promise((resolve, reject) => {
+			db.all<Comment[]>(sql, (err, rows: Comment[]) => {
+				if (err) {
+					console.error(err);
+					reject(err);
+				} else {
+					resolve(rows);
+				}
+			});
+		});
+		return await new Response(JSON.stringify(comments), { status: 200 });
+	} catch (error) {
+		console.error(error);
+		return await new Response(JSON.stringify('Error'), { status: 500 });
+	}
+}
 
-
-    } catch (error ) {
-        console.error(error)
-        return await new Response(JSON.stringify(""), {status: 500});
-    }
+export async function POST(event: RequestEvent) {
+	try {
+		let comment = await event.request.json();
+		let sql = 'INSERT INTO comments (author, content, post_id) VALUES (?,?,?);';
+		db.run(sql, [comment.author, comment.content, comment.post_id]);
+		return await new Response('Success', { status: 200 });
+	} catch (error) {
+		return await new Response('Internal Server Error', { status: 500 });
+	}
 }
