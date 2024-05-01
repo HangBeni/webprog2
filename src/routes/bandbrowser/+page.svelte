9fd9c304a9645c33f4752d3ac782b28f0ef524f6
HangBeni <hangbenjamin2003@gmail.com>
2024-05-01 22:28:09 +0200

    feat: base browsing page

---
+++ src/routes/bandbrowser/+page.svelte
@@ -0,0 +1,24 @@
+
+<script lang="ts">
+	import BandCard from "$lib/bandbrowsing/BandCard.svelte";
+	import type { SelectBand } from "$lib/db/schema";
+	import { onMount } from "svelte";
+
+    let bands : SelectBand[] = [];
+
+onMount(async () => {
+    bands = await fetch('api/band').then(res => res.json())
+} )
+</script>
+
+
+<main>
+    {#each bands as band}
+        <BandCard {band}/>
+    {/each}
+</main>
+
+
+<style>
+
+</style>
