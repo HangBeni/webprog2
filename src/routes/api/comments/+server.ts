0f9198b333ebffc131858c5fc258078829b15a33
HangBeni <hangbenjamin2003@gmail.com>
2024-05-01 14:15:52 +0200

    feat: comment in drizzle

--- src/routes/api/comments/+server.ts
+++ src/routes/api/comments/+server.ts
@@ -1,32 +1,24 @@
-import db from '../../../database/db';
-import type { Comment } from '$lib/types';
+import { db } from '$lib/db/db.server';
+import { comments, type InsertComment, type SelectComment } from '$lib/db/schema';
 import type { RequestEvent } from '@sveltejs/kit';
 
 export async function GET() {
 	try {
-		let sql = 'SELECT * FROM comments;';
-		let comments = await new Promise((resolve, reject) => {
-			db.all<Comment[]>(sql, (err, rows: Comment[]) => {
-				if (err) {
-					console.error(err);
-					reject(err);
-				} else {
-					resolve(rows);
-				}
-			});
-		});
-		return await new Response(JSON.stringify(comments), { status: 200 });
+		
+		const commentForPost : SelectComment[] = await db.select().from(comments);
+
+		return await new Response(JSON.stringify(commentForPost), { status: 200 });
 	} catch (error) {
 		console.error(error);
-		return await new Response(JSON.stringify('Error'), { status: 500 });
+		return await new Response(JSON.stringify({}), { status: 500 });
 	}
 }
 
 export async function POST(event: RequestEvent) {
 	try {
-		let comment = await event.request.json();
-		let sql = 'INSERT INTO comments (author, content, post_id) VALUES (?,?,?);';
-		db.run(sql, [comment.author, comment.content, comment.post_id]);
+		const commentForPost : InsertComment = await event.request.json();
+
+		await db.insert(comments).values(commentForPost).execute();
 		return await new Response('Success', { status: 200 });
 	} catch (error) {
 		return await new Response('Internal Server Error', { status: 500 });
