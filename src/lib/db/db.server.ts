d41fdbb4719279b3c01f6d20a9fd0710cce5643b
HangBeni <hangbenjamin2003@gmail.com>
2024-05-01 15:38:36 +0200

    feat: db schema in for querying

--- src/lib/db/db.server.ts
+++ src/lib/db/db.server.ts
@@ -1,14 +1,17 @@
-import { drizzle, type BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import { bands, posts } from './schema';
+import * as schema from './schema';
 
 const sqlite = new Database('sqlite.db');
-export const db: BetterSQLite3Database = drizzle(sqlite);
+export const db = drizzle(sqlite, { schema });
 
-db.insert(bands)
+await db
+	.insert(bands)
 	.values({ name: 'FFTS', story: 'Baszó emberek', genre: 'Metáál', birth: '2021-01-01' })
 	.onConflictDoNothing({ target: bands.name });
 
-	db.insert(posts)
-	.values({author:'Beni', content:'Az FFTS a legbaszóbb banda' })
+await db
+	.insert(posts)
+	.values({ author: 'Beni', content: 'Az FFTS a legbaszóbb banda' })
 	.onConflictDoNothing({ target: bands.name });
