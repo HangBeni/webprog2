5e13b554d7eaf06acd1353b11b4c23a1bd7b8dce
HangBeni <hangbenjamin2003@gmail.com>
2024-05-01 22:27:30 +0200

    feat: new browsing link and old reg links

--- src/lib/reusables/Nav.svelte
+++ src/lib/reusables/Nav.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	import type { Links } from "$lib/types";
+	import type { Links } from '$lib/types';
 
-
-    export const links : Links[] = [
-    {name: "Főoldal", link: "/"},
-    {name: "Bejelentkezés", link: "/login"}, //Ha be van jelentkezve akkor cserélje ki egy avatarral
-    //Saját bandám bejelentkezés után ha van a usernek
-    ];
+	export const links: Links[] = [
+		{ name: 'Főoldal', link: '/' },
+		{ name: 'Bandák keresése', link: '/bandbrowser' },
+		{ name: 'Bejelentkezés', link: '/login' } //Ha be van jelentkezve akkor cserélje ki egy avatarral
+		//Saját bandám bejelentkezés után ha van a usernek
+	];
 </script>
 
 <nav>
-    <ul>
-        {#each links as link}
-            <li><a href={link.link}>{link.name}</a></li>
-        {/each}
-        <details >
-            <summary>Regisztáció</summary>
-            <a href="/band">Banda regisztráció</a>
-            <a href="/user">User regisztráció</a>
-        </details>
-    </ul>
-</nav> 
+	<ul>
+		{#each links as link}
+			<li><a href={link.link}>{link.name}</a></li>
+		{/each}
+		<details>
+			<summary>Regisztáció</summary>
+			<a href="/registration/band">Banda regisztráció</a>
+			<a href="/registration/user">User regisztráció</a>
+		</details>
+	</ul>
+</nav>
