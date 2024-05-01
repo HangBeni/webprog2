b5b7787feedeced2fc84d8b1833f776545864d39
HangBeni <hangbenjamin2003@gmail.com>
2024-05-01 22:26:01 +0200

    feat: no initial records

--- src/lib/db/db.server.ts
+++ src/lib/db/db.server.ts
@@ -1,17 +1,7 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
-import { bands, posts } from './schema';
 import * as schema from './schema';
 
 const sqlite = new Database('sqlite.db');
 export const db = drizzle(sqlite, { schema });
 
-await db
-	.insert(bands)
-	.values({ name: 'FFTS', story: 'Baszó emberek', genre: 'Metáál', birth: '2021-01-01' })
-	.onConflictDoNothing({ target: bands.name });
-
-await db
-	.insert(posts)
-	.values({ author: 'Beni', content: 'Az FFTS a legbaszóbb banda' })
-	.onConflictDoNothing({ target: bands.name });
