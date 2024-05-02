1b0801d56a4433db4a7b00ecf45d68795ee1baec
HangBeni <hangbenjamin2003@gmail.com>
2024-05-02 19:55:36 +0200

    feat: manage session

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,18 @@
-export async function load() {
-
-
-	return {
-		inSession: true,
-		userID: 1, /*user id from session */
-        userBand: 1
-	};
+export async function load({ cookies }) {
+	//session get
+	const session = cookies.get('session');
+	if (!session) {
+		return {
+			inSession: false,
+			userID: 0 /*user id from session */,
+			userBand: 0
+		};
+	} else {
+		const data = JSON.parse(session) as {userID: number, bandID: number};
+		return {
+			inSession: true,
+			userID: data.userID /*user id from session */,
+			userBand: data.bandID
+		};
+	}
 }
