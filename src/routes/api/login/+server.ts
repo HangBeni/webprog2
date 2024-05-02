f5e6f9a14a963d92c678b85a4a35981195f055ac
HangBeni <hangbenjamin2003@gmail.com>
2024-05-02 19:56:00 +0200

    feat: cookies set and delete

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -1,13 +1,13 @@
 import { db } from '$lib/db/db.server';
-import { users, type SelectUser } from '$lib/db/schema';
+import { users } from '$lib/db/schema';
 import { RegistrationStatus, type User } from '$lib/types';
-import type { RequestEvent } from '@sveltejs/kit';
+import { redirect, type RequestEvent } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 
 export async function POST(event: RequestEvent) {
 	try {
 		const loggerUser: User = await event.request.json();
-
+		
 		const isThere = await db.query.users.findFirst({
 			where:
 				eq(users.name, loggerUser.name) &&
@@ -18,8 +18,25 @@ export async function POST(event: RequestEvent) {
 		if (!isThere) {
 			return new Response(JSON.stringify(RegistrationStatus.NotFound), { status: 422 });
 		}
+
+		event.cookies.set('session', JSON.stringify({userID: isThere.id, bandID: isThere.band_id}), {
+			path:'/',
+			httpOnly: true,
+			sameSite:true,
+			maxAge: 60*60*24*2 //2 napos bejentkezés
+		});
+
 		return new Response(JSON.stringify(RegistrationStatus.ThereIs), { status: 200 });
 	} catch (error) {
 		return new Response(JSON.stringify(RegistrationStatus.ServerFail), { status: 500 });
+	}
+}
+
+export async function DELETE({cookies}) {
+	try {
+		await cookies.delete('session', {path:'/'})
+		throw redirect(303,'/')
+	} catch (error) {
+		throw redirect(303,'/')
 	}
 }
