34e00bd0a1e9f5b7f1e1c6b243cd86738f8c07ea
HangBeni <hangbenjamin2003@gmail.com>
2024-05-03 15:11:28 +0200

    feat: display the posting component if the session is flow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-import Nav from '$lib/reusables/Nav.svelte';
-import type { LayoutData } from './$types';
+	import { page } from '$app/stores';
+	import PostingInput from '$lib/posts/PostingInput.svelte';
+	import Nav from '$lib/reusables/Nav.svelte';
+	import type { LayoutData } from './$types';
 
-export let data:LayoutData;
-
+	export let data: LayoutData;
+    
 </script>
 
-<Nav {data}/>
-<slot/>
+<Nav {data} />
+{#if data.inSession && $page.url.pathname === '/'}
+	<PostingInput />
+{/if}
+<slot />
