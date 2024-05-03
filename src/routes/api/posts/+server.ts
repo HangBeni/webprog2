db4e5b22564a09da733a6ed79fa41bb8ef8aa8cb
HangBeni <hangbenjamin2003@gmail.com>
2024-05-03 23:29:43 +0200

    feat: post like a band or a user

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,5 +1,5 @@
 import { db } from '$lib/db/db.server';
-import { posts, users, type InsertPost } from '$lib/db/schema';
+import { bands, posts, users, type InsertPost } from '$lib/db/schema';
 import type { RequestEvent } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 
@@ -20,7 +20,6 @@ export async function GET() {
 			statusText: 'Posts!',
 			headers: { 'Content-Type': 'application/json' }
 		});
-
 	} catch (error) {
 		return new Response('Server fail!', {
 			status: 500,
@@ -32,11 +31,11 @@ export async function GET() {
 
 export async function POST(event: RequestEvent) {
 	try {
-
-		const content: string = await event.request.json();
+		const data: {content:string, url:string} = await event.request.json();
 		const session = event.cookies.get('session');
 
-		if (!session) { // This is not possible actually
+		if (!session) {
+			// This is not possible actually
 			return new Response('Failed posting', {
 				status: 500,
 				statusText: 'No posting!',
@@ -44,16 +43,25 @@ export async function POST(event: RequestEvent) {
 			});
 		}
 
-		const { userID } = JSON.parse(session) as { userID: number; bandID: number };
-		const writer = await db.query.users.findFirst({
-			where: eq(users.id, userID),
-			columns: { name: true }
-		});
+		const { userID, bandID } = JSON.parse(session) as { userID: number; bandID: number | null };
+		let writer: { name: string; } | undefined;
+		console.log(data.url);
+		if (!bandID || data.url === '/' || data.url === '/[user]') {
+			writer = await db.query.users.findFirst({
+				where: eq(users.id, userID),
+				columns: { name: true }
+			});
+		}else if(bandID){
+			writer = await db.query.bands.findFirst({
+				where: eq(bands.id, bandID),
+				columns: { name: true }
+			});
+		}
 
-		const newPost: InsertPost = { content: content, author: writer!.name };
+		const newPost: InsertPost = { content: data.content, author: writer!.name };
 
 		const post = await db.insert(posts).values(newPost).returning();
-		
+
 		if (!post) {
 			return new Response('Failed posting', {
 				status: 500,
