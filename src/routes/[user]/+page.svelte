a382438dc2d3abcd9ae13f9a7eeff478009a70ae
HangBeni <hangbenjamin2003@gmail.com>
2024-05-03 21:59:38 +0200

    feat: display the current session data

---
+++ src/routes/[user]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageServerData } from "./$types";
+
+
+
+	export let data: PageServerData;
+    const {myUser} = data;
+</script>
+
+<main>
+
+</main>
+
+<style>
+</style>
---
+++ src/routes/myband/[band]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+    const {myBand} = data;
+</script>
+
+<main>
+
+</main>
+
+<style>
+</style>
