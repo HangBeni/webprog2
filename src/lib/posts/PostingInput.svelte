75e945417348b6f97713570a1af149b8608d569f
HangBeni <hangbenjamin2003@gmail.com>
2024-05-03 15:08:42 +0200

    feat: posting component

---
+++ src/lib/posts/PostingInput.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { invalidateAll } from "$app/navigation";
+
+
+	let content: string;
+
+	async function createPost() {
+		await fetch('/api/posts', {
+			method: 'Post',
+			body: JSON.stringify(content),
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			}
+		});
+		content = '';
+		invalidateAll();
+	}
+
+</script>
+
+<form>
+	<label for="newPost">What's in your head?</label>
+	<input
+		type="text"
+		name="newPost"
+		id="newPost"
+		minlength="1"
+		autocomplete="off"
+		bind:value={content}
+		on:keypress={(e) => e.key ==='Enter' ?? createPost}
+	/>
+	<button type="submit" on:click={createPost}>--></button>
+</form>
+
+<style>
+</style>
