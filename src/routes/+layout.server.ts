489f5ff534710323840451b2f33e3da540fd2b6e
HangBeni <hangbenjamin2003@gmail.com>
2024-05-05 09:41:45 +0200

    feat: comments out

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,17 +1,18 @@
+
 export async function load({ cookies }) {
 	//session get
 	const session = cookies.get('session');
 	if (!session) {
 		return {
 			inSession: false,
-			userID: 0 /*user id from session */,
+			userID: 0 ,
 			userBand: 0
 		};
 	} else {
 		const data = JSON.parse(session) as {userID: number, bandID: number | null};
 		return {
 			inSession: true,
-			userID: data.userID /*user id from session */,
+			userID: data.userID ,
 			userBand: data.bandID
 		};
 	}
