74108624a9300d72debc3e3aab3332e87946a0c7
HangBeni <hangbenjamin2003@gmail.com>
2024-05-05 09:38:17 +0200

    feat: bands style

--- src/lib/bandbrowsing/BandCard.svelte
+++ src/lib/bandbrowsing/BandCard.svelte
@@ -6,6 +6,23 @@ export let band: SelectBand;
 
 </script>
 
+<div>
+	<h1>{band.name}</h1>
+	<p>{band.story}</p>
+</div>
 
-<h1>{band.name}</h1>
-<p>{band.story}</p>
+
+<style>
+	p{
+		max-width: 20rem;
+	}
+	div{
+		padding: 1rem;
+		padding-right:0rem;
+		margin-top: 1.5rem;
+		width: 27rem;
+		margin-inline: auto;
+		border-radius: 1.5rem;
+		background-color: var(--secondary-color);
+	}
+</style>
