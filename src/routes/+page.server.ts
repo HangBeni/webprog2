69f1e37cec51593a55b87f2f63054706c31ad3e4
HangBeni <hangbenjamin2003@gmail.com>
2024-05-05 09:42:13 +0200

    feat: check the insession from parent

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,11 @@
-import { db } from "$lib/db/db.server";
-import { posts, type SelectPost } from "$lib/db/schema";
+import { db } from '$lib/db/db.server';
+import { posts, type SelectPost } from '$lib/db/schema';
 
-export async function load() {
-    const all : SelectPost[] = await db.select().from(posts);
-    return {
-        all
-    }
+export async function load({ parent }) {
+	const { inSession } = await parent();
+	const all: SelectPost[] = await db.select().from(posts);
+	return {
+		all,
+		inSession
+	};
 }
